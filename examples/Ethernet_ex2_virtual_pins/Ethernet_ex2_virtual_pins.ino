d03141e5558433587363707ea62d2788103a0975
iliaslamprou <iliaslamprou@users.noreply.github.com>
2019-09-27 00:45:03 +0300

    Add files via upload

--- examples/Ethernet_ex2_virtual_pins/Ethernet_ex2_virtual_pins.ino
+++ examples/Ethernet_ex2_virtual_pins/Ethernet_ex2_virtual_pins.ino
@@ -116,13 +116,14 @@ String onRequested(char variableType, uint8_t variableIndex){
         if (debug) Serial.write(c);
       }
      String* response= virtuino.getResponse();    // get the text that has to be sent to Virtuino as reply. The library will check the inptuBuffer and it will create the response text
-     if (debug) Serial.println("\nResponse : "+*response);
-     client.print(*response);
+     if (response->length()>0) {
+        if (debug) Serial.println("\nResponse : "+*response);
+        client.print(*response);
+     }
      client.stop(); 
     }
     if (debug) Serial.println("Disconnected");
   }
  }
-
  //============================================================== vDelay
   void vDelay(int delayInMillis){long t=millis()+delayInMillis;while (millis()<t) virtuinoRun();}
